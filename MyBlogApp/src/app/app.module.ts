3985196d8feb2b506d1fbe708a77d8e66d850c96
Tushar1up1 <tusharup@gmail.com>
2020-09-03 16:26:34 -0400

    user input and display output using two way databinding - ngModel and adding formsModule

--- MyBlogApp/src/app/app.module.ts
+++ MyBlogApp/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { AppBlogCreateComponent } from './blogs/blog-create/blog-create.component'
+import { BlogCreateComponent } from './blogs/blog-create/blog-create.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppBlogCreateComponent
+    BlogCreateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- MyBlogApp/src/app/blogs/blog-create/blog-create.component.ts
+++ MyBlogApp/src/app/blogs/blog-create/blog-create.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
 
+
 @Component({
     selector: 'app-blog-create',
     templateUrl: './blog-create.component.html'
 })
-export class AppBlogCreateComponent {
-    newBlog = '';   
-    addBlog(InputBlog : HTMLTextAreaElement){
-        console.log(InputBlog);
-        console.dir(InputBlog);
-        this.newBlog = InputBlog.value;
-
+export class BlogCreateComponent {
+    newBlog = '';
+    enteredValue = '';   
+    addBlog(){
+        this.newBlog = this.enteredValue;
     }
 }
